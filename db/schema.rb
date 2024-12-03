1d7640f2eceada6f9c9573928bb574dd01a0387f
egihasdi <egi.hasdi@gmail.com>
2024-12-03 16:21:20 +0700

    add user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_03_090351) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_03_091524) do
   create_table "stocks", force: :cascade do |t|
     t.string "symbol", null: false
     t.string "company_name", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_03_090351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["symbol"], name: "index_stocks_on_symbol", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "wallets", force: :cascade do |t|
