96ada877e214d6fed2a656ea603b1913c2b0175a
egihasdi <egi.hasdi@gmail.com>
2024-12-04 12:48:59 +0700

    prevent transfer record if transferring insuficient amount

--- app/models/transfer_transaction.rb
+++ app/models/transfer_transaction.rb
@@ -1,10 +1,21 @@
 # Transfer moves funds from the source_wallet to the target_wallet
 class TransferTransaction < Transaction
+  validate :sufficient_balance
+
   def apply
     unless source_wallet && target_wallet
       raise "Both source and target wallets must be present for a transfer transaction"
     end
 
-    self.save!
+    ApplicationRecord.transaction do
+      raise "Insufficient balance in source wallet" unless source_wallet.balance >= amount
+      self.save!
+    end
+  end
+
+  def sufficient_balance
+    if source_wallet.balance < amount
+      errors.add(:amount, "Insufficient balance in source wallet")
+    end
   end
 end
